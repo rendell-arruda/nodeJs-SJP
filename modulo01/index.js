5ed1b668e7cb6c5db5aca1b02690715ffaf5bd78
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-20 15:57:40 -0300

    query params and route params

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,10 +4,22 @@ const express = require('express');
 //iniciar o express
 const server = express();
 
+//query params = sao parametros nomeados enviados na rota apos o simbolo de interrogacao e servem para filtros, paginacao
+//ex query params: ...?nome=Diego
+
+//route params = sao parametros utilizados para identificar recursos
+//ex route params: .../users/1
+
+//request body = corpo da requisicao, utilizado para criar ou alterar recursos
+//ex request body: { "name": "Diego", "idade": 23 }
+
 //criar rotas
 //req representa os dados que vem do cliente e res representa a resposta que vai ser enviada para o cliente
-server.get('/cursos', (req, res) => {
-  return res.json({ curso: 'Node JS' });
+server.get('/cursos/:id', (req, res) => {
+  const nome = req.query.nome;
+  const id = req.params.id;
+  return res.json({ curso: `Curso ${id}` });
+  //   return res.json({ curso: `Aprendendo ${nome}` });
 });
 
 //rodar em uma porta
